6d795ca01ed35cfffd09c595ea52a0891dc8c018
Matt <matthew.morley.ca@gmail.com>
2020-04-20 15:52:09 -0700

    use dev wpilib

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -1,26 +1,8 @@
 package frc.robot
 
-import edu.wpi.first.wpilibj.DoubleSolenoid
-import frc.robot.subsystems.superstructure.Hood
-import frc.robot.subsystems.superstructure.Intake
 import org.ghrobotics.lib.wrappers.FalconTimedRobot
 
 object Robot : FalconTimedRobot() {
-
-    override fun robotInit() {
-        +Intake
-        +Hood
-    }
-
-    override fun teleopInit() {
-        Intake.solenoid.set(DoubleSolenoid.Value.kForward)
-        Intake.motor.set(1.0)
-    }
-
-    override fun disabledInit() {
-        Intake.solenoid.set(DoubleSolenoid.Value.kReverse)
-        Intake.motor.set(0.0)
-    }
 
 }
 
---
+++ src/test/kotlin/RobotTest.kt
@@ -0,0 +1,9 @@
+import edu.wpi.first.wpilibj.RobotBase
+import frc.robot.Robot
+import org.junit.Test
+
+class RobotTest {
+    @Test fun testInit() {
+        Robot.start()
+    }
+}
